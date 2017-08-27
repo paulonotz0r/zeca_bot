b4b054609afdbe9e7a5667fc5f3427a178d39d84
cauez0rd <caue_bc@yahoo.com.br>
2017-08-27 13:41:59 -0300

    Add /signo method, comment out /all til I figure whats wrong

--- bin/zeca_bot.rb
+++ bin/zeca_bot.rb
@@ -10,14 +10,13 @@ duda = ' <a href="tg://user?id=215491130">Duda</a>'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-
       when '/oi'
         bot.api.send_message(chat_id: message.chat.id, text: "Vai tomar no cu, meo!")
 
       when '/meta'
         bot.api.send_message(chat_id: message.chat.id, text: "Tem que meter o louco!")
 
-      when '/penis' 
+      when '/penis'
         bot.api.send_message(chat_id: message.chat.id, text: "Tá querendo chupar um né viado")
 
       when '/lucifer'
@@ -26,14 +25,16 @@ Telegram::Bot::Client.run(token) do |bot|
       when '/viado'
         bot.api.send_message(chat_id: message.chat.id, text: "E aí #{message.from.first_name}, beleza?")
 
-      when '/all'
-        bot.api.send_message(chat_id: message.chat.id, parse_mode: "HTML", 
-          text: bruna + matheuzinho + ale + duda)
+     # when '/all'
+       # bot.api.send_message(chat_id: message.chat.id, parse_mode: "HTML", text: bruna + matheuzinho + ale + duda)
+
+      when '/signo'
+        bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, meu signo é **VAI TOMAR NO CU FILHO DA PUTA EU SOU UM FODENDO CACHORRO E VOCÊ QUER ME DAR SIGNO SEU ARROMBADO**")
 
       when '/start'
         question = 'London is a capital of which country?'
         # See more: https://core.telegram.org/bots/api#replykeyboardmarkup
-         answers = 
+         answers =
              Telegram::Bot::Types::ReplyKeyboardMarkup
              .new(keyboard: [%w(Teste1 Teste2), %w(Teste3 Teste4)], one_time_keyboard: true)
                 bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
@@ -42,8 +43,7 @@ Telegram::Bot::Client.run(token) do |bot|
          # See more: https://core.telegram.org/bots/api#replykeyboardremove
         kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
             bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
-
-
      end
+
   end
 end
